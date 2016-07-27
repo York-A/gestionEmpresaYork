c3223b90e4195a2e7f601e9864ac12c2e3f80c6a
AYork <yorkdam@gmail.com>
2016-07-27 09:38:41 +0200

    log

---
+++ gestionempresa/src/main/java/com/ipartek/formacion/pojo/Empleado.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ipartek.formacion.pojo;
+
+import java.util.Date;
+
+/**
+ * @author Curso
+ *
+ */
+public class Empleado {
+
+	public static final int CODIGO_EMPLEADO=-1;
+	private int codEmp;
+	private String nombreEmp;
+	private String apellidosEmp;
+	private String dni_nie;
+	private String n_ss;
+	private String cCorriente;
+	private String direccion;
+	private String localidad;
+	private int cPostal;
+	private Date fNacimiento;
+	private Date fContratacion;
+	private int dptoEmp;
+}
